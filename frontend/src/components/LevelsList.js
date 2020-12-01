f4b35e224bcad3bf740cff4f799c60b5d654b589
Victor Peixoto <victorlevipeixoto@gmail.com>
2020-12-01 19:31:53 -0300

    Create list for new areas

---
+++ frontend/src/components/LevelsList.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+
+const LevelsList = ({ levels, removeProgram, updateProgram }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+
+  return levels.map((level, index) => (
+    <div
+			style={{marginLeft: 'auto', marginRight: 'auto'}}
+      className={'level-row'}
+      key={level.id}
+    >
+      <div style={{display: 'flex', justifyContent: 'space-between', flex: '5', marginRight: '30px'}}key={level.id}>
+        <div>
+          {level.name}
+        </div>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'space-between', flex: '5', marginRight: '30px'}}key={level.id}>
+				<div>
+					{level.lvlMin}
+				</div>
+				<div>
+					{level.mobsLvl}
+				</div>
+      </div>
+      <div className='icons' style={{flex: 1}}>
+        <RiCloseCircleLine
+          onClick={() => removeProgram(level.id)}
+          className='delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: level.id, value: {date: level.date, time: level.time, duration: level.duration, name: level.name} })}
+          className='edit-icon' style={{flex: 1}}
+        />
+      </div>
+    </div>
+  ));
+};
+
+export default LevelsList;
+
